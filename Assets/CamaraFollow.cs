93ff8eb08396a97de0f81d92e2b5f189e11a89c2
Mondei1 <emailchecker000@gmail.com>
2018-06-01 12:40:10 +0200

    Add camara follow with lerp.

---
+++ Assets/CamaraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CamaraFollow : MonoBehaviour {
+
+    public Transform player;
+
+    public float smoothLevel = .15f;
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        Vector3 normalPos = player.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, normalPos, smoothLevel * 100 * Time.deltaTime);
+        transform.position = smoothedPos;
+    }
+
+}
